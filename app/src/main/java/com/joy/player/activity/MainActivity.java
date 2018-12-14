9d040c327d5e254e3d75cdd1cdd8ca37c6a1471c
joy <joy@finereport.com>
2018-12-14 10:20:24 +0800

    second commit

---
+++ app/src/main/java/com/joy/player/activity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.joy.player.musicplayer.activity;
+
+import android.os.Bundle;
+import com.joy.player.musicplayer.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView();
+    }
+}
---
+++ app/src/main/java/com/joy/player/activity/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.joy.player.musicplayer.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
