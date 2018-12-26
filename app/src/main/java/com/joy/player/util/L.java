d117d2daa8b88902de341835336db746d81a7e4c
joy <joy@finereport.com>
2018-12-26 18:11:28 +0800

    添加日志类

---
+++ app/src/main/java/com/joy/player/util/L.java
@@ -0,0 +1,17 @@
+package com.joy.player.util;
+
+import android.util.Log;
+
+/**
+ */
+public class L {
+    public static void D(boolean print, String tag, String content) {
+        if (print)
+            Log.d(tag, content);
+    }
+
+    public static void E(boolean print, String tag, String content) {
+        if (print)
+            Log.e(tag, content);
+    }
+}
